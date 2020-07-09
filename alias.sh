4c6d53fe49368554de9c09038a3d5135381072c0
Yuan <yuanz@hpe.com>
2020-07-09 12:28:11 +0800

    init

--- alias.sh
+++ alias.sh
@@ -5,8 +5,8 @@ echo "alias kg='kubectl -n iot-gateway'" >> ~/.bashrc
 echo "alias kw='kubectl -n iot-workloads'" >> ~/.bashrc
 echo "alias ka='kubectl -A'" >> ~/.bashrc
 
-echo "alias psql='k exec -n iot-gateway -it ii-se-postgresql-0 -- sh -c \'PGPASSWORD=123456 psql database\''" >> ~/.bashrc
-echo "alias redis-cli='k exec -n iot-gateway -it ii-se-redis-0 -- redis-cli -a 123456'" >> ~/.bashrc
-echo "etcdctl() { k exec -n iot-gateway -it ii-se-monitor-etcd-0 -- sh -c 'ETCDCTL_API=3 etcdctl $*'; }" >> ~/.bashrc
+echo "alias psql=\"k exec -n iot-gateway -it ii-se-postgresql-0 -- sh -c 'PGPASSWORD=123456 psql database'\"" >> ~/.bashrc
+echo "alias redis-cli=\"k exec -n iot-gateway -it ii-se-redis-0 -- redis-cli -a 123456\"" >> ~/.bashrc
+echo "etcdctl() { k exec -n iot-gateway -it ii-se-monitor-etcd-0 -- sh -c \"ETCDCTL_API=3 etcdctl $*\"; }" >> ~/.bashrc
 
 source ~/.bashrc
